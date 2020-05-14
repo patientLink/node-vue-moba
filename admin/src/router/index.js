ec21f967584bae3900e7d9a07624f5f86257f135
patientLink <pc353240229@gmail.com>
2020-05-14 16:41:07 +0800

    first commit

---
+++ admin/src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from 'views/Main.vue'
+import CategoryEdit from 'views/CategoryEdit'
+import CategoryList from 'views/CategoryList'
+
+import ItemEdit from 'views/ItemEdit'
+import ItemList from 'views/ItemList'
+
+import HeroEdit from 'views/HeroEdit'
+import HeroList from 'views/HeroList'
+
+import ArticleEdit from 'views/ArticleEdit'
+import ArticleList from 'views/ArticleList'
+
+import AdEdit from 'views/AdEdit'
+import AdList from 'views/AdList'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Main',
+    component: Main,
+    children: [
+      { path: '/categories/create', component: CategoryEdit },
+      { path: '/categories/edit/:id', component: CategoryEdit, props: true },
+      { path: '/categories/list', component: CategoryList },
+
+      { path: '/items/create', component: ItemEdit },
+      { path: '/items/edit/:id', component: ItemEdit, props: true },
+      { path: '/items/list', component: ItemList },
+
+      { path: '/heroes/create', component: HeroEdit },
+      { path: '/heroes/edit/:id', component: HeroEdit, props: true },
+      { path: '/heroes/list', component: HeroList },
+
+      { path: '/articles/create', component: ArticleEdit },
+      { path: '/articles/edit/:id', component: ArticleEdit, props: true },
+      { path: '/articles/list', component: ArticleList },
+      
+      { path: '/ads/create', component: AdEdit },
+      { path: '/ads/edit/:id', component: AdEdit, props: true },
+      { path: '/ads/list', component: AdList },
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
